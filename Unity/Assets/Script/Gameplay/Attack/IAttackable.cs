a519cce00d47aea3f6d79306be3203ec8aede0eb
Simon <le6monstre@hotmail.com>
2024-04-13 19:58:40 -0400

    Remove unused delegate

--- Unity/Assets/Script/Gameplay/Attack/IAttackable.cs
+++ Unity/Assets/Script/Gameplay/Attack/IAttackable.cs
@@ -4,7 +4,6 @@ namespace Game
 {
     public interface IAttackable
     {
-        delegate void DeathHandler(IAttackable attackable);
         public event Action<Attack, IAttackable> OnDamageTaken;
 
         public void TakeAttack(Attack attack);
