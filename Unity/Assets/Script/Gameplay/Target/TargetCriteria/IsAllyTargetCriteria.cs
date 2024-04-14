c7c0df4d39d2ab3aef44770870b8342b18fb9ad8
Simon <le6monstre@hotmail.com>
2024-04-14 13:20:12 -0400

    Reimplement Shaman and Alchemist abilities

--- Unity/Assets/Script/Gameplay/Target/TargetCriteria/IsAllyTargetCriteria.cs
+++ Unity/Assets/Script/Gameplay/Target/TargetCriteria/IsAllyTargetCriteria.cs
@@ -7,7 +7,7 @@ namespace Game
     {
         public override bool Execute(ITargeteable owner, ITargeteable targeteable, object caller)
         {
-            return targeteable.Faction == targeteable.Faction;
+            return owner.Faction == targeteable.Faction;
         }
     }
 }
