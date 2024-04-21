43fc541925694f0f3f5a677ece3137d482139430
Simon <le6monstre@hotmail.com>
2024-04-21 17:31:30 -0400

    Missing perk assets

---
+++ Unity/Assets/Script/Gameplay/Agent/Technology/Definition/Shieldbearer/DamageAgainstWeakPerk.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace Game
+{
+    [CreateAssetMenu(fileName = "DamageAgainstWeakPerk", menuName = "Definition/Technology/Shieldbearer/DamageAgainstWeakPerk")]
+    public class DamageAgainstWeakPerk : CharacterTechnologyPerkDefinition
+    {
+        public class Modifier : Modifier<Modifier, DamageAgainstWeakPerk>
+        {
+            public override float? DamageDealtAgainstWeak => definition.damageDealtAgainstWeak;
+
+            public Modifier(IModifiable modifiable, DamageAgainstWeakPerk modifierDefinition) : base(modifiable, modifierDefinition)
+            {
+            }
+        }
+
+        [SerializeField] private float damageDealtAgainstWeak;
+
+        public override Game.Modifier GetModifier(IModifiable modifiable)
+        {
+            return new Modifier(modifiable, this);
+        }
+    }
+}
---
+++ Unity/Assets/Script/Gameplay/Agent/Technology/Definition/Shieldbearer/RangedReductionDamagePerk.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace Game
+{
+    [CreateAssetMenu(fileName = "RangedReductionDamagePerk", menuName = "Definition/Technology/Shieldbearer/RangedReductionDamagePerk")]
+    public class RangedReductionDamagePerk : CharacterTechnologyPerkDefinition
+    {
+        public class Modifier : Modifier<Modifier, RangedReductionDamagePerk>
+        {
+            public override float? RangedDamageReduction => definition.rangedDamageReduction;
+
+            public Modifier(IModifiable modifiable, RangedReductionDamagePerk modifierDefinition) : base(modifiable, modifierDefinition)
+            {
+            }
+        }
+
+        [SerializeField, Range(0, 1)] private float rangedDamageReduction;
+
+        public override Game.Modifier GetModifier(IModifiable modifiable)
+        {
+            return new Modifier(modifiable, this);
+        }
+    }
+}
