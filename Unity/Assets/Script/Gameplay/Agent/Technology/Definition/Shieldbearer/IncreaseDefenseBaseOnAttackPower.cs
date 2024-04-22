84f16ffa4e763865fca828ad988815cd6307c8f6
le6monstre <le6montre@hotmail.com>
2024-04-22 19:54:37 -0400

    Perk: Increase defense base on attack power

---
+++ Unity/Assets/Script/Gameplay/Agent/Technology/Definition/Shieldbearer/IncreaseDefenseBaseOnAttackPower.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+namespace Game
+{
+    [CreateAssetMenu(fileName = "IncreaseDefenseBaseOnAttackPower", menuName = "Definition/Technology/Shieldbearer/IncreaseDefenseBaseOnAttackPower")]
+    public class IncreaseDefenseBaseOnAttackPower : CharacterTechnologyPerkDefinition
+    {
+        public class Modifier : Modifier<Modifier, IncreaseDefenseBaseOnAttackPower>
+        {
+            public override float? Defense => Mathf.Floor(modifiable.GetCachedComponent<Character>().AttackPower / definition.attackPowerRequired) * definition.defenseIncrease;
+
+            public Modifier(IModifiable modifiable, IncreaseDefenseBaseOnAttackPower modifierDefinition) : base(modifiable, modifierDefinition)
+            {
+            }
+        }
+
+        [SerializeField] private float defenseIncrease;
+        [SerializeField] private float attackPowerRequired;
+
+        public override string ParseDescription()
+        {
+            return $"Increase Defense by {defenseIncrease} for each {attackPowerRequired}";
+        }
+
+        public override Game.Modifier GetModifier(IModifiable modifiable)
+        {
+            return new Modifier(modifiable, this);
+        }
+    }
+}
