0d8ddc383712826aa14d1f083b8c75b13803ab55
Simon <le6monstre@hotmail.com>
2024-07-20 19:40:08 -0400

    - Add seer perk that increase attack power base on the amount of buff applied to enemies

---
+++ Unity/Assets/Script/Gameplay/Agent/Technology/Definition/Seer/IncreaseDamageBaseOnEffectAppliedPerk.cs
@@ -0,0 +1,34 @@
+﻿using System.Linq;
+using UnityEngine;
+
+namespace Game
+{
+    [CreateAssetMenu(fileName = "IncreaseDamageBaseOnEffectAppliedPerk", menuName = "Definition/Technology/Seer/IncreaseDamageBaseOnEffectAppliedPerk")]
+    public class IncreaseDamageBaseOnEffectAppliedPerk : CharacterTechnologyPerkDefinition
+    {
+        public class Modifier : Modifier<Modifier, IncreaseDamageBaseOnEffectAppliedPerk>
+        {
+            public override float? AttackPower => definition.attackPowerPerEffectApplied * Source.AppliedModifiers.Count(x =>
+                x.Definition is not CharacterTechnologyPerkDefinition
+                && x.Modifiable.TryGetCachedComponent<ITargeteable>(out ITargeteable target)
+                && Source.TryGetCachedComponent<ITargeteable>(out ITargeteable sourceTarget)
+                && target.Faction != sourceTarget.Faction);
+
+            public Modifier(IModifiable modifiable, IncreaseDamageBaseOnEffectAppliedPerk modifierDefinition, IModifierSource source) : base(modifiable, modifierDefinition, source)
+            {
+            }
+        }
+
+        [SerializeField] private float attackPowerPerEffectApplied;
+
+        public override Game.Modifier GetModifier(IModifiable modifiable)
+        {
+            return new Modifier(modifiable, this, modifiable.GetCachedComponent<IModifierSource>());
+        }
+
+        public override string ParseDescription()
+        {
+            return string.Format(Description, attackPowerPerEffectApplied);
+        }
+    }
+}
