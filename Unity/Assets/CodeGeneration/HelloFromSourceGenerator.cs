ab42162156e0c9135e950906bffee8e94e22b83f
Simon <le6monstre@hotmail.com>
2024-09-01 11:14:17 -0400

    - Add Hello World code genreation

---
+++ Unity/Assets/CodeGeneration/HelloFromSourceGenerator.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class HelloFromSourceGenerator : MonoBehaviour
+{
+    static string GetStringFromSourceGenerator()
+    {
+        return ExampleSourceGenerated.ExampleSourceGenerated.GetTestText();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        var output = "Test";
+
+        output = GetStringFromSourceGenerator();
+        Debug.Log(output);
+    }
+}
