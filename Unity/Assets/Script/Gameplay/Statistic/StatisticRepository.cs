73a231b7c889fcd5c6539e05c74dc09838073039
Simon <le6monstre@hotmail.com>
2024-09-22 12:37:51 -0400

    - Fix statistic memory allocation

--- Unity/Assets/Script/Gameplay/Statistic/StatisticRepository.cs
+++ Unity/Assets/Script/Gameplay/Statistic/StatisticRepository.cs
@@ -1,10 +1,17 @@
 ﻿
 using Game;
+using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
 public static class StatisticRepository
 {
+    [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.SubsystemRegistration)]
+    static void Init()
+    {
+        statisticDefinitions = null;
+    }
+
     public static string Damage = "damage";
     public static string MaxHealth = "max_health";
     public static string Health = "health";
@@ -20,10 +27,20 @@ public static class StatisticRepository
     public static string Speed = "speed";
     public static string Heal = "heal";
 
+    public static List<StatisticDefinition> statisticDefinitions = null;
+
     public static StatisticDefinition GetDefinition(string name)
     {
-        StatisticDefinition[] statisticDefinitions = Resources.LoadAll<StatisticDefinition>("Definition/Statistic");
-        return statisticDefinitions.First(x => x.HumanReadableId == name);
+        if (statisticDefinitions == null)
+            statisticDefinitions = Resources.LoadAll<StatisticDefinition>("Definition/Statistic").ToList();
+
+        foreach (StatisticDefinition statisticDefinition in statisticDefinitions)
+        {
+            if (statisticDefinition.HumanReadableId == name)
+                return statisticDefinition;
+        }
+
+        throw new System.Exception($"Unable to find the statistic with the name {name}");
     }
 }
 
