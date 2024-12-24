b2454c894d82ad18b33aeaf03c96a0099fc2aa23
Simon <le6monstre@hotmail.com>
2024-12-24 11:34:08 -0500

    Increase AttackSpeed by amount of stagger applied

---
+++ Unity/Assets/Script/Gameplay/Entities/Modifier/Triggers/OnModifierAppliedModifierTrigger.cs
@@ -0,0 +1,36 @@
+﻿using Game.Character;
+using System;
+using System.Linq;
+using UnityEngine;
+
+namespace Game.Modifier
+{
+    [Serializable]
+    public class OnModifierAppliedModifierTrigger : ModifierTrigger
+    {
+        [SerializeField] private ModifierDefinition modifierDefinition;
+
+        private CharacterEntity character;
+        private ModifierApplier applier;
+
+        public override void Initialize(ModifierEntity modifier)
+        {
+            base.Initialize(modifier);
+            character = modifier.GetHierarchy().FirstOrDefault(x => x is CharacterEntity) as CharacterEntity;
+            applier = character.GetCachedComponent<ModifierApplier>();
+            applier.OnChildModifierApplied += OnChildModifierApplied;
+        }
+
+        private void OnChildModifierApplied(ModifierEntity modifier)
+        {
+            if (modifier.Definition == modifierDefinition)
+                Trigger();
+        }
+
+        public override void Dispose()
+        {
+            base.Dispose();
+            applier.OnChildModifierApplied -= OnChildModifierApplied;
+        }
+    }
+}
