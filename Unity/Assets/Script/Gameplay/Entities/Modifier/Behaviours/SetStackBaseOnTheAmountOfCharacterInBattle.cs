a2c198406bc6bb2d37a6ff9a18aecc010c5ae16b
Simon <le6monstre@hotmail.com>
2024-12-24 13:03:54 -0500

    Increase attack power base on the amount of allies on the battle ground.

---
+++ Unity/Assets/Script/Gameplay/Entities/Modifier/Behaviours/SetStackBaseOnTheAmountOfCharacterInBattle.cs
@@ -0,0 +1,41 @@
+﻿using Game.Character;
+using Game.Components;
+using System;
+using System.Linq;
+using UnityEngine;
+
+namespace Game.Modifier
+{
+    [Serializable]
+    public class SetStackBaseOnTheAmountOfCharacterInBattle : ModifierBehaviour
+    {
+        [SerializeReference, SubclassSelector] private ModifierTargetFilter filter;
+
+        private StackModifierBehaviour stackModifierBehaviour;
+
+        public override void Initialize(ModifierEntity modifier)
+        {
+            base.Initialize(modifier);
+            stackModifierBehaviour = modifier.Behaviours.OfType<StackModifierBehaviour>().FirstOrDefault();
+            filter.Initialize(modifier);
+        }
+
+        public override Result Update()
+        {
+            int stack = 0;
+            foreach (Target target in Target.All)
+            {
+                if (target.Entity is not CharacterEntity)
+                    continue;
+
+                if (!filter.Execute(target.Entity))
+                    continue;
+
+                stack++;
+            }
+
+            stackModifierBehaviour.SetStack(stack);
+            return base.Update();
+        }
+    }
+}
