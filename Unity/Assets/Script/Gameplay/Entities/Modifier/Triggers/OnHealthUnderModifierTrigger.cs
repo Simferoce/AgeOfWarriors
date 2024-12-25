d112655a18870553f9b7c3db380b680b6577ddb3
Simon <le6monstre@hotmail.com>
2024-12-25 17:52:03 -0500

    Increase attack power when low health berserker.

---
+++ Unity/Assets/Script/Gameplay/Entities/Modifier/Triggers/OnHealthUnderModifierTrigger.cs
@@ -0,0 +1,34 @@
+﻿using Game.Statistics;
+using System;
+using UnityEngine;
+
+namespace Game.Modifier
+{
+    [Serializable]
+    public class OnHealthUnderModifierTrigger : ModifierTrigger
+    {
+        [SerializeField] private StatisticReference threshold;
+
+        private bool triggered = false;
+
+        public override void Initialize(ModifierEntity modifier)
+        {
+            base.Initialize(modifier);
+            threshold.Initialize(modifier);
+        }
+
+        public override void Update()
+        {
+            base.Update();
+
+            if (triggered)
+                return;
+
+            if (modifier.Target.Entity[StatisticDefinitionRegistry.Instance.Health] / modifier.Target.Entity[StatisticDefinitionRegistry.Instance.MaxHealth] < threshold.Get())
+            {
+                Trigger();
+                triggered = true;
+            }
+        }
+    }
+}
