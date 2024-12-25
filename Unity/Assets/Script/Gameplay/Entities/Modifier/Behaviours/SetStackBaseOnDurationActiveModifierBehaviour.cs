fe3c469c81a4cffea0f99dca44943cbeed49ff3a
Simon <le6monstre@hotmail.com>
2024-12-25 11:22:45 -0500

    Relentless Push Technology for the Berserker

---
+++ Unity/Assets/Script/Gameplay/Entities/Modifier/Behaviours/SetStackBaseOnDurationActiveModifierBehaviour.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+using UnityEngine;
+
+namespace Game.Modifier
+{
+    [Serializable]
+    public class SetStackBaseOnDurationActiveModifierBehaviour : ModifierBehaviour
+    {
+        private StackModifierBehaviour stackModifierBehaviour;
+        private float startedAt;
+
+        public override void Initialize(ModifierEntity modifier)
+        {
+            base.Initialize(modifier);
+            startedAt = Time.time;
+            stackModifierBehaviour = modifier.Behaviours.OfType<StackModifierBehaviour>().FirstOrDefault();
+        }
+
+        public override Result Update()
+        {
+            stackModifierBehaviour.SetStack((int)(Time.time - startedAt));
+            return base.Update();
+        }
+    }
+}
