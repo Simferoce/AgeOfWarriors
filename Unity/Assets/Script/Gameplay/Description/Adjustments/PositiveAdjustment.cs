4054458cba010043d8425a292c805a92bb45120f
Simon <le6monstre@hotmail.com>
2024-12-26 14:54:09 -0500

    Reduce defense on attack seer technology

---
+++ Unity/Assets/Script/Gameplay/Description/Adjustments/PositiveAdjustment.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using UnityEngine;
+
+namespace Game
+{
+    [Serializable]
+    public class PositiveAdjustment : Adjustment<float>, IFloatAdjustment
+    {
+        public override float Adjust(float value)
+        {
+            return Mathf.Abs(value);
+        }
+    }
+}
