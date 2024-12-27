006a231b8d5edf3179ecc958efbcd56517c24243
Simon <le6monstre@hotmail.com>
2024-12-26 22:15:12 -0500

    Reduce speed by 25% when in contact with pool seer technology

---
+++ Unity/Assets/Script/Gameplay/Entities/Modifier/TargetFilters/IsPoolTargetFilter.cs
@@ -0,0 +1,19 @@
+﻿using Game.Pool;
+using System;
+
+namespace Game.Modifier
+{
+    [Serializable]
+    public class IsPoolTargetFilter : ModifierTargetFilter
+    {
+        public override void Initialize(ModifierEntity modifier)
+        {
+            base.Initialize(modifier);
+        }
+
+        public override bool Execute(Entity target)
+        {
+            return target is PoolEntity;
+        }
+    }
+}
---
+++ Unity/Assets/Script/Gameplay/Entities/Modifier/Triggers/OnSubEntityCreateModifierTrigger.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game.Modifier
+{
+    [Serializable]
+    public class OnSubEntityCreateModifierTrigger : ModifierTrigger, IModifierTargetProvider
+    {
+        [SerializeReference, SubclassSelector] private ModifierTargetFilter targetFilter;
+
+        private Entity target;
+
+        public override void Initialize(ModifierEntity modifier)
+        {
+            base.Initialize(modifier);
+
+            modifier.Target.Entity.OnDescendantAdded += Entity_OnDescendantAdded;
+        }
+
+        private void Entity_OnDescendantAdded(Entity entity)
+        {
+            if (targetFilter.Execute(entity))
+            {
+                target = entity;
+                Trigger();
+            }
+        }
+
+        public override void Dispose()
+        {
+            base.Dispose();
+
+            modifier.Target.Entity.OnDescendantAdded -= Entity_OnDescendantAdded;
+        }
+
+        public List<object> GetTargets()
+        {
+            return new List<object>() { target };
+        }
+    }
+}
