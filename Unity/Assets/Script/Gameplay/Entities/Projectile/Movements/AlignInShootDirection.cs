932ce6f12627030ae8e8180a737f12757d71a412
Simon <le6monstre@hotmail.com>
2025-02-22 13:31:17 -0500

    Poison Stalker implementation

---
+++ Unity/Assets/Script/Gameplay/Entities/Projectile/Movements/AlignInShootDirection.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+using UnityEngine;
+
+namespace Game.Projectile
+{
+    [Serializable]
+    public class AlignInShootDirection : ProjectileMovement
+    {
+        private float direction;
+
+        public override void Initialize(ProjectileEntity projectile)
+        {
+            base.Initialize(projectile);
+
+            ProjectileParameter<float> projectileParameter = projectile.Parameters.OfType<ProjectileParameter<float>>().FirstOrDefault(x => x.Name == "direction");
+            if (projectileParameter == null)
+            {
+                Debug.LogError($"Could not find a parameter with the name \"direction\".");
+                return;
+            }
+
+            direction = projectileParameter.GetValue();
+        }
+
+        public override void Update()
+        {
+            projectile.transform.right = Vector3.right * direction;
+        }
+    }
+}
